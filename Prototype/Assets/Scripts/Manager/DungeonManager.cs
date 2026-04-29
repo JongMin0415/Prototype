447c948f6e39c1e4d6710b4b773ff3a8ed1b1c80
JongMin0415 <leejm0415@naver.com>
2026-04-30 00:54:35 +0900

    fix : " 던전 중복방 수정 "

--- Prototype/Assets/Scripts/Manager/DungeonManager.cs
+++ Prototype/Assets/Scripts/Manager/DungeonManager.cs
@@ -6,10 +6,14 @@ public class DungeonManager : MonoBehaviour
 {
     public Room startRoom;
 
-    public GameObject[] normalRooms;
+    [Header("Room Pools")]
+    public GameObject[] easyRooms;
+    public GameObject[] mediumRooms;
+    public GameObject[] hardRooms;
+
     public GameObject bossRoom;
 
-    public int roomCount = 5;
+    public int roomCount = 6;
     public float roomWidth = 20f;
 
     private List<GameObject> spawnedRooms = new List<GameObject>();
@@ -22,23 +26,35 @@ public class DungeonManager : MonoBehaviour
     void GenerateDungeon()
     {
         Vector2 position = startRoom.transform.position;
-
         Room prevRoom = startRoom;
+
+        List<GameObject> easyPool = new List<GameObject>(easyRooms);
+        List<GameObject> mediumPool = new List<GameObject>(mediumRooms);
+        List<GameObject> hardPool = new List<GameObject>(hardRooms);
 
         for (int i = 0; i < roomCount - 2; i++)
         {
             position += new Vector2(roomWidth, 0);
 
-            GameObject newRoomObj = Instantiate(
-                normalRooms[Random.Range(0, normalRooms.Length)],
-                position,
-                Quaternion.identity
-            );
+            GameObject roomPrefab = null;
 
+            if (i == 0)
+            {
+                roomPrefab = GetRandomRoom(easyPool, easyRooms);
+            }
+            else if (i <= 2)
+            {
+                roomPrefab = GetRandomRoom(mediumPool, mediumRooms);
+            }
+            else
+            {
+                roomPrefab = GetRandomRoom(hardPool, hardRooms);
+            }
+
+            GameObject newRoomObj = Instantiate(roomPrefab, position, Quaternion.identity);
             spawnedRooms.Add(newRoomObj);
 
             Room newRoom = newRoomObj.GetComponent<Room>();
-
             prevRoom.nextRoomPoint = newRoom.entryPoint;
 
             prevRoom = newRoom;
@@ -50,7 +66,20 @@ public class DungeonManager : MonoBehaviour
         spawnedRooms.Add(bossObj);
 
         Room bossRoomScript = bossObj.GetComponent<Room>();
-
         prevRoom.nextRoomPoint = bossRoomScript.entryPoint;
+    }
+
+    GameObject GetRandomRoom(List<GameObject> pool, GameObject[] originalArray)
+    {
+        if (pool.Count == 0)
+        {
+            pool.AddRange(originalArray);
+        }
+
+        int index = Random.Range(0, pool.Count);
+        GameObject selected = pool[index];
+        pool.RemoveAt(index);
+
+        return selected;
     }
 }
